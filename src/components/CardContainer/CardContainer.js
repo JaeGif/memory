ad153bc9a48a0e4a579331a8c03a143d05f59022
Jae <giffordjacob0@gmail.com>
2022-10-27 19:02:15 +0800

    cards styled and responsive

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Card from '../Card/Card';
+import styles from '../CardContainer/CardContainer.module.css';
 
 const CardContainer = ({ cardArray }) => {
   const cards = cardArray.map((char) => <Card key={char.id} card={char} />);
 
-  return <>{cards}</>;
+  return <div className={styles.cardContainers}>{cards}</div>;
 };
+
 export default CardContainer;
