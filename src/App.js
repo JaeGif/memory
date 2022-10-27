864ca3233c1c8b27d4dfdac9870705c09b8ce81d
Jae <giffordjacob0@gmail.com>
2022-10-27 14:55:51 +0800

    assets added, structure modeled in app.js and components initializated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      {/* Header
+          Scoreboard
+          Lose/Win Conditionally rendered 
+          CardContainer -- cards are stored in an array of components and the array is randomly filled with components after each click
+                           thus changing where components are rendered. 
+                           
+            Cards   <Cards contain .png and name>
+            Cards
+            Cards
+            ...
+          
+          Footer*/}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Loss/Loss.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Loss() {
+  return <div>Loss</div>;
+}
