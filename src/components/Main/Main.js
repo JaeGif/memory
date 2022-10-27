6a24a85e78c03a2545ad792b724f9453bb1c3eff
Jae <giffordjacob0@gmail.com>
2022-10-27 20:19:01 +0800

    rerenders cards only on click

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,19 +6,24 @@ import CardContainer from '../CardContainer/CardContainer';
 import { useState } from 'react';
 import defaultCardArray from './defaults';
 import shuffle from '../shuffle';
+import { click } from '@testing-library/user-event/dist/click';
 
 export default function Main() {
   const [isLost, setIsLost] = useState(false);
+  const [score, setScore] = useState(0);
 
-  const [cardArray, setCardArray] = useState([]);
-  useEffect(() => setCardArray(shuffle(defaultCardArray)), []);
+  let [cardArray, setCardArray] = useState(defaultCardArray);
 
   const randomizeCards = () => {
-    setCardArray(shuffle(cardArray));
+    setCardArray([...shuffle(cardArray)]);
   };
 
   const onCardClick = () => {
+    randomizeCards();
+
     console.log('made it dad');
+    console.log(score);
+
     // check that the card has NOT yet been clicked.
     // if it has, lose, else
     // increment score and
